a8963c44f06d5a58628a42bc2342fd424c800574
PedroCazelatto <pedrocazelatto@gmail.com>
2022-07-05 13:59:45 -0300

    Screen size based on terminal size

--- cmdLine.py
+++ cmdLine.py
@@ -1,31 +1,34 @@
+import os
+
 from rich import box
-from rich.align import Align
-from rich.box import DOUBLE
 from rich.style import Style
-from rich.panel import Panel
 from rich.console import RenderableType
-from rich.tree import Tree
 from rich.text import Text
 from rich.table import Table
+from rich.layout import Layout
 
 from textual import events
 from textual.reactive import Reactive
 from textual.widget import Widget
 
-from memoryApp import memoryApps
-from folderOpen import folderOpen
+from memoryApps import memoryApps
+from interface import interface
 
-class cmdLine(Widget):
-    title = ""
+class _cmdLine(Widget):
+    _instance = None
+    
+    cmdHeight= 3
     line = Reactive(Text("cmd> "))
     cmdText = ""
     history = list()
     i = 0
+    ignoreKeys = ["ctrl+a",
+                  "ctrl+r",
+                  "left",
+                  "right",
+                  "ctrl+i",
+                  ]
     
-    def __init__(self, title: str):
-        super().__init__(title)
-        self.title = title
-        
     def commands(self, cmd: iter):
         if len(cmd) < 1:
             return
@@ -33,12 +36,16 @@ class cmdLine(Widget):
         # if len(cmdSplited) == 1:
         #     return
         if cmd[0] == "load":
+            # chamar loader
             memoryApps().addApp(cmd[1])
+            interface().refresher()
         elif cmd[0] == "assemble":
             # chamar assembler
             pass
         elif cmd[0] == "simulate":
-            pass
+            interface().changeMode("Simulation")
+        elif cmd[0] == "home":
+            interface().changeMode("Home")
     
     def on_focus(self) -> None:
         self.line = Text("cmd> ").append(self.cmdText).append("_", style=Style(blink=True))
@@ -47,12 +54,10 @@ class cmdLine(Widget):
         self.line = self.line[:-1]
         
     def on_key(self, event: events.Key):
-        if event.key == "ctrl+h":
-            self.cmdText = self.cmdText[:-1]
-        elif event.key == "ctrl+a" or event.key == "left" or event.key == "right":
+        if self.ignoreKeys.count(event.key) == 1:
             pass
-        elif event.key == "ctrl+r":
-            folderOpen().updater()
+        elif event.key == "ctrl+h":
+            self.cmdText = self.cmdText[:-1]
         elif event.key == "enter":
             if not self.cmdText.isspace():
                 self.history.append(self.cmdText)
@@ -72,16 +77,25 @@ class cmdLine(Widget):
         self.line = Text("cmd> ").append(self.cmdText).append("_", style=Style(blink=True))
 
     def render(self) -> RenderableType:
+        height = int(os.get_terminal_size()[1]/4)
+        height = min(height, 13)
+        height = max(height, 3)
+        height -= 3
     
         grid = Table(show_header= False,
                      expand= True,
                      box= box.HEAVY,
-                     style= Style(color= "green"))
+                     style= Style(color= "green1", bold= True))
         
-        for i in range(4):
-            if i >= 4-len(self.history):
-                grid.add_row(self.history[-4+i])
+        for i in range(height):
+            if i >= height - len(self.history):
+                grid.add_row(self.history[-height + i])
             else:
                 grid.add_row("")
         grid.add_row(self.line)
         return grid
+
+def cmdLine():
+    if _cmdLine._instance is None:
+        _cmdLine._instance = _cmdLine()
+    return _cmdLine._instance
