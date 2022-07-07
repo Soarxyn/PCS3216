bb2fcfd577e8dc6940e29b45a44ac7581d9bd391
Aimnos <roikegami@hotmail.com>
2022-07-07 20:09:22 -0300

    Fixed Windows and macOS commands

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@ if [ "$1" == "-d" ]; then
     cargo build
     case "$(uname -s)" in
         Linux*)     ln -sf ./target/debug/libsisprog.so ./sisprog.so;;
-        Darwin*)    ln -sf ./target/debug/libsisprog.dylib ./sisprog.dylib;;
-        *)          ln -sf ./target/debug/libsisprog.dll ./sisprog.dll;;
+        Darwin*)    ln -sf ./target/debug/libsisprog.dylib ./sisprog.so;;
+        *)          ln -sf ./target/debug/sisprog.dll ./sisprog.pyd;;
     esac
 else
     cargo build --release
     case "$(uname -s)" in
         Linux*)     ln -sf ./target/release/libsisprog.so ./sisprog.so;;
-        Darwin*)    ln -sf ./target/release/libsisprog.dylib ./sisprog.dylib;;
-        *)          ln -sf ./target/release/libsisprog.dll ./sisprog.dll;;
+        Darwin*)    ln -sf ./target/release/libsisprog.dylib ./sisprog.so;;
+        *)          ln -sf ./target/release/sisprog.dll ./sisprog.pyd;;
     esac
 fi
