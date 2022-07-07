ce2952af68ad53dfbbf8e01e5c32486ec0602bb8
PedroCazelatto <pedrocazelatto@gmail.com>
2022-07-06 21:48:34 -0300

    Added commands

--- helpList.py
+++ helpList.py
@@ -8,10 +8,14 @@ from textual.widget import Widget
 
 class helpList(Widget):
     
-    helpContents = ["[b]ASSEMBLE [i]arquivo[/]",
+    helpContents = ["[b]ASSEMBLE [i]arquivo[/i] \[-o saida][/]",
+                    "[b]LINK [i]arquivos[/i] \[-o saida][/]",
                     "[b]LOAD [i]arquivo[/]",
-                    "[b]SIMULATE [i]arquivo[/]",
                     "[b]HOME[/]",
+                    "[b]SIMULATE [i]arquivo[/]",
+                    "[b]RUN[/]",
+                    "[b]PEEK [i]arquivo[/]",    #IMPLEMENTAR
+                    "[b]UNLOAD [i]arquivo[/]",  #IMPLEMENTAR
                     ]
     
     helpBar = Tree("Comandos")
@@ -20,6 +24,6 @@ class helpList(Widget):
         helpBar.add(_)
     
     def render(self) -> RenderableType:
-        return Panel(Align.left(self.helpBar),
+        return Panel(Align(self.helpBar),
                      title= "Comandos",
                      border_style= Style(color= "bright_magenta"))
